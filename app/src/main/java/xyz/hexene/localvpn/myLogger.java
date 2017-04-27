c30a0a252be52d60f921afaae269ac8609cb5d51
Brandon Sande <bransande@gmail.com>
2017-04-27 12:32:56 -0500

    Number of Connections added to Log

--- app/src/main/java/xyz/hexene/localvpn/myLogger.java
+++ app/src/main/java/xyz/hexene/localvpn/myLogger.java
@@ -2,6 +2,7 @@ package xyz.hexene.localvpn;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 /**
  * Created by Brandon on 4/27/2017.
@@ -11,7 +12,7 @@ public class myLogger implements Runnable {
     String ipAddress;
     Context context;
     SharedPreferences sharedPreferences;
-    int count;
+    int count, s_count;
 
     public myLogger(String ip, Context cont){
         this.ipAddress = ip;
@@ -22,10 +23,13 @@ public class myLogger implements Runnable {
     public void run() {
         sharedPreferences = context.getSharedPreferences("ipAddressTable", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        String [] s_ipAddress = ipAddress.split("-");
+        String [] s_ipAddress = ipAddress.split(":");
+        s_count = sharedPreferences.getInt(s_ipAddress[0], 0);
+        s_count++;
 
-        editor.putInt(s_ipAddress[0], count++);
+        editor.putInt(s_ipAddress[0], s_count);
         editor.commit();
+        Log.d("MyLogger", s_ipAddress[0] + ": " + s_count);
      //   GlobalVars gv = (GlobalVars) context;
      //   gv.addIpAddress(s_ipAddress[0]);
     }
